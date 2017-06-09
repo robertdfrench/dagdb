420eb0fc59544342c2942c86e947cbe5d8f32c69
Robert D. French <robert@robertdfrench.me>
2017-06-09 14:19:02 -0500

    Setup barebones python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+        name='DagDB',
+        version='2.0.0',
+        description='DAG Service backed by a relational database or other indexable data store',
+        url='https://github.com/robertdfrench/dagdb',
+        packages=['dagdb']
+        )
