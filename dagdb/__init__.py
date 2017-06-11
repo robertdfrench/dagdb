01b8e2848aed3a9714a2ba90c8aebb2b114ac259
Robert D. French <robert@robertdfrench.me>
2017-06-10 19:23:21 -0500

    Find node by name

--- dagdb/__init__.py
+++ dagdb/__init__.py
@@ -14,17 +14,22 @@ def clean():
 class DatabaseClient(object):
     def __init__(self, db_path):
         self.dbh = sqlite3.connect(db_path)
-        self.dbh.execute("CREATE TABLE IF NOT EXISTS nodes (content TEXT)")
+        self.dbh.execute("CREATE TABLE IF NOT EXISTS nodes (name TEXT, content TEXT)")
 
     @property
     def num_nodes(self):
         rv = self.dbh.execute("SELECT COUNT(*) FROM nodes")
         return rv.fetchone()[0]
 
-    def insert(self, node):
-        self.dbh.execute("INSERT INTO nodes (content) VALUES ('%s')" % json.dumps(node))
+    def insert(self, name, node):
+        self.dbh.execute("INSERT INTO nodes (name, content) VALUES ('%s', '%s')" % (name, json.dumps(node)))
         self.dbh.commit()
 
     def find_all(self):
         rv = self.dbh.execute("SELECT * FROM nodes")
-        return [json.loads(row[0]) for row in rv.fetchall()]
+        return [json.loads(row[1]) for row in rv.fetchall()]
+
+    def find(self, name):
+        rv = self.dbh.execute("SELECT * FROM nodes WHERE name='%s'" % name)
+        row = rv.fetchone()
+        return json.loads(row[1])
--- test/test_dag.py
+++ test/test_dag.py
@@ -18,26 +18,33 @@ def test_zero_nodes_in_empty_db(db):
 
 def test_can_insert_node(db):
     node = dict(poop=True)
-    db.insert(node)
+    db.insert('junk', node)
 
 
 def test_insert_increases_node_count(db):
     node = dict(poop=True)
-    db.insert(node)
+    db.insert('junk', node)
     assert db.num_nodes == 1
 
 
 def test_can_find_node(db):
     node = dict(poop=True)
-    db.insert(node)
+    db.insert('junk', node)
     nodes = db.find_all()
     assert nodes[0] == node
 
 def test_can_persist_nodes_between_sessions():
     first_session = dagdb.new()
     node = dict(poop=True)
-    first_session.insert(node)
+    first_session.insert('junk', node)
 
     second_session = dagdb.new()
     nodes = second_session.find_all()
     assert nodes[0] == node
+
+
+def test_retrieve_node_by_id(db):
+    db.insert('shitline', dict(happy='roo'))
+
+    node = db.find('shitline')
+    assert node['happy'] == 'roo'
